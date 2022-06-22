155518bc2ff2ce03fb8f2f39099b28b2b6eb447c
Vagner Batista <vagnereletronica@gmail.com>
2022-06-22 15:41:11 -0300

    ci: atualizado next.config.js

--- components/cards/cat/CatCard.mocks.ts
+++ components/cards/cat/CatCard.mocks.ts
@@ -1,7 +1,11 @@
 import { ICatCard } from "./CatCard";
 
 const base:ICatCard = {
-    sampleTextProp: 'Sample text!'
+    title: 'Card de gatos!',
+    tag: 'gatos',
+    author: 'Vagner Batista',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!',
+    time: '2 horas atrás'
 }
 
 export const mockCatCardProps = {
--- components/templates/base/BaseTemplate.tsx
+++ components/templates/base/BaseTemplate.tsx
@@ -2,7 +2,7 @@ import styles from './BaseTemplate.module.css';
 
 export interface IBaseTemplate{
     sampleTextProp: String
-};
+}
 
 const BaseTemplate:React.FC<IBaseTemplate> = ({sampleTextProp}) => {
     return <div className={styles.component}>{sampleTextProp}</div>
