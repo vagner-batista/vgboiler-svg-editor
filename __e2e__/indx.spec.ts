9b75e4d297f6ec3fa61c0ac1e3b7ab8ec271d563
Vagner Batista <vagnereletronica@gmail.com>
2022-06-24 16:08:07 -0300

    ci: com playwright

---
+++ __e2e__/indx.spec.ts
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+
+test.beforeEach(async ({page}) => {
+    await page.goto('http://localhost:3000/');
+})
+
+test.describe('Ir para página', () => {
+    test("Contém header", async ({page})=>{
+        await page.goto('http://localhost:3000/sobre')
+    })
+})
