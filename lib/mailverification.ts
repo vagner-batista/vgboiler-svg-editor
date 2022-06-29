c809099911df6fe115773e8d8c5a5b46415b8f1d
Vagner Batista <vagnereletronica@gmail.com>
2022-06-29 12:47:59 -0300

    ci: implementados providers github e email no next-auth

---
+++ lib/mailverification.ts
@@ -0,0 +1,57 @@
+// Cria o texto do email de verificação
+function createHtml({
+  url,
+  host,
+  email,
+}: Record<'url' | 'host' | 'email', string>) {
+  // Escapa email e hostname para que usuário não se confunda com onde deve clicar
+  const escapedEmail = `${email.replace(/\./g, '&#8203;.')}`;
+  const escapedHost = `${host.replace(/\./g, '&#8203;.')}`;
+
+  // Some simple styling options
+  const backgroundColor = '#f9f9f9';
+  const textColor = '#444444';
+  const mainBackgroundColor = '#ffffff';
+  const buttonBackgroundColor = '#346df1';
+  const buttonBorderColor = '#346df1';
+  const buttonTextColor = '#ffffff';
+
+  return `
+<body style="background: ${backgroundColor};">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td align="center" style="padding: 10px 0px 20px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+        <strong>${escapedHost}</strong>
+      </td>
+    </tr>
+  </table>
+  <table width="100%" border="0" cellspacing="20" cellpadding="0" style="background: ${mainBackgroundColor}; max-width: 600px; margin: auto; border-radius: 10px;">
+    <tr>
+      <td align="center" style="padding: 10px 0px 0px 0px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+        Faça login como <strong>${escapedEmail}</strong>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" style="padding: 20px 0;">
+        <table border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="center" style="border-radius: 5px;" bgcolor="${buttonBackgroundColor}"><a href="${url}" target="_blank" style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${buttonTextColor}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${buttonBorderColor}; display: inline-block; font-weight: bold;">Login</a></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td align="center" style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+        Se você não solicitou este e-mail pode ignorá-lo com segurança.
+      </td>
+    </tr>
+  </table>
+</body>
+`;
+}
+
+function createText({ url, host }: Record<'url' | 'host', string>) {
+  return `Faça login em ${host}\n${url}\n\n`;
+}
+
+export { createHtml, createText };
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,20 +1,78 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { NextApiHandler } from "next";
-import NextAuth from "next-auth/next";
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { NextApiHandler } from 'next';
+import { NextAuthOptions } from 'next-auth';
+import NextAuth from 'next-auth/next';
+import EmailProvider from 'next-auth/providers/email';
 import GithubProvider from 'next-auth/providers/github';
-import prisma from "../../../lib/prisma";
+import nodemailer from 'nodemailer';
+import { createHtml, createText } from '../../../lib/mailverification';
+import prisma from '../../../lib/prisma';
 
-const authHandler:NextApiHandler = (req, res) => NextAuth(req, res, options);
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 
-const options = {
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        })
-    ],
-    adapter: PrismaAdapter(prisma),
-    secret: process.env.SECRET
-}
+const options: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: parseInt(process.env.EMAIL_SERVER_PORT || '587'),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+      sendVerificationRequest: async function sendVerificationRequest({
+        identifier: email,
+        url,
+        provider: { server, from },
+      }) {
+        const { host } = new URL(url);
+        const transport = nodemailer.createTransport(server);
+        await transport.sendMail({
+          to: email,
+          from,
+          subject: `Efetue login em ${host}`,
+          text: createText({ url, host }),
+          html: createHtml({ url, host, email }),
+        });
+      },
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+  //callbacks: {
+  //  async signIn({ user, account, profile, email, credentials }) {
+  //    return true
+  //  },
+  //  async redirect({ url, baseUrl }) {
+  //    return baseUrl
+  //  },
+  //  async session({ session, token, user }) {
+  //    return session
+  //  },
+  //  async jwt({ token, user, account, profile, isNewUser }) {
+  //    return token
+  //  }
+  //},
+  //events: {
+  //  async signIn(message) { /* on successful sign in */ },
+  //  async signOut(message) { /* on signout */ },
+  //  async createUser(message) { /* user created */ },
+  //  async updateUser(message) { /* user updated - e.g. their email was verified */ },
+  //  async linkAccount(message) { /* account (e.g. Twitter) linked to a user */ },
+  //  async session(message) { /* session is active */ },
+  //},
+  theme: {
+    colorScheme: 'auto', // "auto" | "dark" | "light"
+    brandColor: '#f18739', // Hex color code
+    logo: 'https://github.com/vagner-batista.png', // Absolute URL to image
+  },
+  useSecureCookies: true,
+};
 
 export default authHandler;
